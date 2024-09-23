0266b7b9ca4e4448eee66d2813cebc38aa0a8bd0
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-09-23 23:39:10 +0800

    first commit

---
+++ TestMacApp/TestMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMacAppApp.swift
+//  TestMacApp
+//
+//  Created by L Af on 2024/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TestMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
